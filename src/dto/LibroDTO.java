e00d15406959edb495aec15c0aba5eb7efc9772d
FranBecerra06 <fbecerragarcia1704@gmail.com>
2025-06-13 23:49:25 +0200

    Actualizacion LibroDTO

--- src/dto/LibroDTO.java
+++ src/dto/LibroDTO.java
@@ -2,4 +2,25 @@ package dto;
 
 public class LibroDTO {
 
+	private int idLibro;
+	private String titulo;
+	private int anio;
+	public int getIdLibro() {
+		return idLibro;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public int getAnio() {
+		return anio;
+	}
+	
+	public String toString() {
+		return "LibroDTO [idLibro=" + idLibro + ", titulo=" + titulo + ", anio=" + anio + "]";
+	}
+	
+	
+	
+	
+	
 }
