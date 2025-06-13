ed445a05bc6a34a4b6f07dd4cfc13a175ad16728
FranBecerra06 <fbecerragarcia1704@gmail.com>
2025-06-13 23:49:12 +0200

    Actualizacion AutorDTO

--- src/dto/AutorDTO.java
+++ src/dto/AutorDTO.java
@@ -2,4 +2,24 @@ package dto;
 
 public class AutorDTO {
 
+	private int idAutor;
+	private String nombre;
+	private String pais;
+
+	public int getIdAutor() {
+		return idAutor;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public String toString() {
+		return "AutorDTO [idAutor=" + idAutor + ", nombre=" + nombre + ", pais=" + pais + "]";
+	}
+
 }
