be327fb95a2d5b08b8ddac6b0bd6a9c7eca237d3
FranBecerra06 <fbecerragarcia1704@gmail.com>
2025-06-14 01:07:28 +0200

    Actualizacion Clase AutorDAO

--- src/dto/AutorDTO.java
+++ src/dto/AutorDTO.java
@@ -6,8 +6,26 @@ public class AutorDTO {
 	private String nombre;
 	private String pais;
 
+	public AutorDTO(int idAutor, String nombre, String pais) {
+		this.idAutor = idAutor;
+		this.nombre = nombre;
+		this.pais = pais;
+	}
+
 	public int getIdAutor() {
 		return idAutor;
+	}
+
+	public void setIdAutor(int idAutor) {
+		this.idAutor = idAutor;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
 	}
 
 	public String getNombre() {
