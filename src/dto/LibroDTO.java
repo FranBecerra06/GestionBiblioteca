fdf62b51fa5bc499e6e9248cabc60619ed847a58
FranBecerra06 <fbecerragarcia1704@gmail.com>
2025-06-14 01:07:55 +0200

    Actualizacion Clase LibroDTO

--- src/dto/LibroDTO.java
+++ src/dto/LibroDTO.java
@@ -5,22 +5,40 @@ public class LibroDTO {
 	private int idLibro;
 	private String titulo;
 	private int anio;
+
+	public LibroDTO(int idLibro, String titulo, int anio) {
+
+		this.idLibro = idLibro;
+		this.titulo = titulo;
+		this.anio = anio;
+	}
+
 	public int getIdLibro() {
 		return idLibro;
 	}
+
+	public void setIdLibro(int idLibro) {
+		this.idLibro = idLibro;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public int getAnio() {
 		return anio;
 	}
-	
+
 	public String toString() {
 		return "LibroDTO [idLibro=" + idLibro + ", titulo=" + titulo + ", anio=" + anio + "]";
 	}
-	
-	
-	
-	
-	
+
 }
