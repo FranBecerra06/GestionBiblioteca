ba94c32d9d68e824a292d8ffebae8fd49461ce41
FranBecerra06 <fbecerragarcia1704@gmail.com>
2025-06-15 17:41:05 +0200

    Actualizacion clase Conexion

--- src/util/Conexion.java
+++ src/util/Conexion.java
@@ -3,39 +3,22 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.*;
 
 public class Conexion {
 
-	private static final String URL = "jdbc:mysql://localhost:3306/biblioteca";
-	private static final String USER = "root";
-	private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
 
-	public static Connection obtenerConexion() {
-
-		Connection conexion = null;
-
-		try {
-
-			conexion = DriverManager.getConnection(URL, USER, PASSWORD);
-			System.out.println("¡Conexión exitosa a la base de datos!");
-
-		} catch (SQLException e) {
-
-			System.out.println("No se ha podido conectar con la base de datos.");
-
-			e.printStackTrace();
-
-		} finally {
-			try {
-				if (conexion != null && !conexion.isClosed()) {
-					conexion.close();
-					System.out.println("Conexión cerrada correctamente.");
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return conexion;
-	}
+    public static Connection obtenerConexion() {
+        Connection conexion = null;
+        try {
+            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("¡Conexión exitosa a la base de datos!");
+        } catch (SQLException e) {
+            System.out.println("No se ha podido conectar con la base de datos.");
+            e.printStackTrace();
+        }
+        return conexion;
+    }
 }
